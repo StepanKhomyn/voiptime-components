c19422377f9a6544f9a21219372bdbc8097dca83
Volodymyr Khomyn <vkhomyn95@gmail.com>
2025-09-17 10:59:56 +0300

    fix: іконки дзвінка

---
+++ src/icons/others/MinusIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="24" height="24" rx="12" fill="white" fill-opacity="0.4" />
+    <path
+      d="M9 12H15"
+      stroke="currentColor"
+      stroke-width="1.7"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
