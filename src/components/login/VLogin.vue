83c6d77ddb593837982e9070886832b1da373f1a
Stepan Khomyn <skhomyn@voiptime.net>
2026-03-05 14:25:08 +0200

    fix: took off uppercase in login component in password

--- src/components/login/VLogin.vue
+++ src/components/login/VLogin.vue
@@ -1,65 +1,64 @@
 <script lang="ts" setup>
-import {reactive} from 'vue';
-import {containUpperCaseLetter, minLength, noSpaces, required, useValidate} from '@/validation';
-import VInput from '@/components/input/VInput.vue';
-import VButton from '@/components/button/VButton.vue';
-import {LOCALE_KEYS} from '@/locales/types';
-import {useI18n} from '@/locales/useI18n';
-import VRadiobox from '@/components/radiobox/VRadiobox.vue';
-import type {VAuthentificationData, VAuthentificationEmits, VAuthentificationProps} from '@/components/login/types';
+  import { reactive } from 'vue';
+  import { minLength, noSpaces, required, useValidate } from '@/validation';
+  import VInput from '@/components/input/VInput.vue';
+  import VButton from '@/components/button/VButton.vue';
+  import { LOCALE_KEYS } from '@/locales/types';
+  import { useI18n } from '@/locales/useI18n';
+  import VRadiobox from '@/components/radiobox/VRadiobox.vue';
+  import type { VAuthentificationData, VAuthentificationEmits, VAuthentificationProps } from '@/components/login/types';
 
-const {t} = useI18n();
+  const { t } = useI18n();
 
-/* -------------------- Props -------------------- */
-const props = withDefaults(defineProps<VAuthentificationProps>(), {
-  loading: false,
-  privacyPolicyUrl: '#',
-  forgotPasswordUrl: '#',
-});
+  /* -------------------- Props -------------------- */
+  const props = withDefaults(defineProps<VAuthentificationProps>(), {
+    loading: false,
+    privacyPolicyUrl: '#',
+    forgotPasswordUrl: '#',
+  });
 
-/* -------------------- Emits -------------------- */
+  /* -------------------- Emits -------------------- */
 
-const emit = defineEmits<VAuthentificationEmits>();
+  const emit = defineEmits<VAuthentificationEmits>();
 
-/* -------------------- State -------------------- */
+  /* -------------------- State -------------------- */
 
-const form = reactive<VAuthentificationData>({
-  username: '',
-  password: '',
-  remember: false,
-});
+  const form = reactive<VAuthentificationData>({
+    username: '',
+    password: '',
+    remember: false,
+  });
 
-/* -------------------- Validation -------------------- */
+  /* -------------------- Validation -------------------- */
 
-const rules = {
-  username: [required, minLength(3)],
-  password: [required, minLength(3), containUpperCaseLetter, noSpaces],
-};
+  const rules = {
+    username: [required, minLength(3)],
+    password: [required, minLength(3), noSpaces],
+  };
 
-const v$ = useValidate(rules, form);
+  const v$ = useValidate(rules, form);
 
-/* -------------------- Methods -------------------- */
+  /* -------------------- Methods -------------------- */
 
-const submit = async () => {
-  v$.$touch();
-  const isValid = await v$.$validate();
-  if (isValid) {
-    const payload = {
-      username: form.username,
-      password: form.password,
-      remember: form.remember
-    } as VAuthentificationData;
-    emit('auth', payload);
-  }
-};
+  const submit = async () => {
+    v$.$touch();
+    const isValid = await v$.$validate();
+    if (isValid) {
+      const payload = {
+        username: form.username,
+        password: form.password,
+        remember: form.remember,
+      } as VAuthentificationData;
+      emit('auth', payload);
+    }
+  };
 </script>
 
 <template>
   <div class="vt-login">
     <div class="vt-login__wrapper">
-
       <div class="vt-login__logo">
-        <slot name="logo"/>
+        <slot name="logo" />
       </div>
 
       <div class="vt-login__greeting">
@@ -67,77 +66,62 @@ const submit = async () => {
       </div>
 
       <div v-loader="props.loading" class="vt-login__form">
-
         <div class="vt-login__form-item">
-          <div
-              class="vt-login__form-label vt-login__form-label__required"
-          >
+          <div class="vt-login__form-label vt-login__form-label__required">
             {{ t(LOCALE_KEYS.LOGIN_USERNAME) }}
           </div>
           <VInput
-              v-model="form.username"
-              :error-message="Array.isArray(v$.username?.$errors) ? v$.username.$errors[0]?.$message : undefined"
-              outlined
-              @keyup.enter="submit"
+            v-model="form.username"
+            :error-message="Array.isArray(v$.username?.$errors) ? v$.username.$errors[0]?.$message : undefined"
+            outlined
+            @keyup.enter="submit"
           />
         </div>
 
         <div class="vt-login__form-item">
-          <div
-              class="vt-login__form-label vt-login__form-label__required"
-          >
+          <div class="vt-login__form-label vt-login__form-label__required">
             {{ t(LOCALE_KEYS.LOGIN_PASSWORD) }}
           </div>
           <VInput
-              v-model.trim="form.password"
-              :error-message="Array.isArray(v$.password?.$errors) ? v$.password.$errors[0]?.$message : undefined"
-              :show-password="true"
-              outlined
-              type="password"
-              @keyup.enter="submit"
+            v-model.trim="form.password"
+            :error-message="Array.isArray(v$.password?.$errors) ? v$.password.$errors[0]?.$message : undefined"
+            :show-password="true"
+            outlined
+            type="password"
+            @keyup.enter="submit"
           />
         </div>
 
         <div class="vt-login__form-item">
-          <div
-              class="vt-login__form-label"
-          >
+          <div class="vt-login__form-label">
             {{ t(LOCALE_KEYS.LOGIN_LANGUAGE) }}
           </div>
-          <slot name="language-select"/>
+          <slot name="language-select" />
         </div>
 
         <div class="vt-login__form-item vt-login__form-item--row">
-          <VRadiobox v-model="form.remember" :label="t(LOCALE_KEYS.LOGIN_REMEMBER)"/>
-          <a
-              :href="props.forgotPasswordUrl"
-              class="vt-login__link"
-          >
+          <VRadiobox v-model="form.remember" :label="t(LOCALE_KEYS.LOGIN_REMEMBER)" />
+          <a :href="props.forgotPasswordUrl" class="vt-login__link">
             {{ t(LOCALE_KEYS.LOGIN_FORGET_PASSWORD) }}
           </a>
         </div>
 
         <div class="vt-login__form-item">
-          <VButton
-              :disabled="props.loading"
-              class="vt-login__form-submit"
-              type="primary"
-              @click="submit"
-          >
+          <VButton :disabled="props.loading" class="vt-login__form-submit" type="primary" @click="submit">
             {{ t(LOCALE_KEYS.LOGIN_SUBMIT) }}
           </VButton>
         </div>
 
         <div class="vt-login__form-item vt-login__form-item--center">
-          <a :href="props.privacyPolicyUrl" class="vt-login__link"
-             target="_blank">{{ t(LOCALE_KEYS.LOGIN_PRIVACY_POLICY) }}</a>
+          <a :href="props.privacyPolicyUrl" class="vt-login__link" target="_blank">{{
+            t(LOCALE_KEYS.LOGIN_PRIVACY_POLICY)
+          }}</a>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use './login';
+  @use './login';
 </style>
