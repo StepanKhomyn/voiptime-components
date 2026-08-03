8d65bedccf734037b50c0e6aa2e9c14bcc44d70f
okobel <okobel@voiptime.net>
2026-08-03 16:43:56 +0300

    fix: add image fullscreen component

---
+++ src/components/imageFullscreen/VImageFullscreen.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const isFullscreen = ref(false);
+
+  const toggleFullscreen = () => {
+    isFullscreen.value = !isFullscreen.value;
+  };
+</script>
+
+<template>
+  <div class="vt-image-fullscreen">
+    <div class="vt-image-fullscreen__preview" @click="toggleFullscreen">
+      <slot name="image"></slot>
+    </div>
+
+    <div v-if="isFullscreen" class="vt-image-fullscreen__overlay" @click="toggleFullscreen">
+      <div class="vt-image-fullscreen__content">
+        <slot name="image"></slot>
+      </div>
+    </div>
+  </div>
+</template>
